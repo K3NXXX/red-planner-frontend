bc60307b3411c3d8598c7c368b317844f5a63f26
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2024-04-12 14:48:54 +0300

    module: fixed module import

--- src/app/i/time-blocking/hooks/useTimeBlockDnd.ts
+++ src/app/i/time-blocking/hooks/useTimeBlockDnd.ts
@@ -1,54 +1,54 @@
 import {
-	DragEndEvent,
-	KeyboardSensor,
-	PointerSensor,
-	useSensor,
-	useSensors
-} from '@dnd-kit/core'
-import { arrayMove } from '@dnd-kit/sortable'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { Dispatch, SetStateAction } from 'react'
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Dispatch, SetStateAction } from "react";
 
-import { ITimeBlockResponse } from '@/types/time-block.types'
+import { ITimeBlockResponse } from "@/types/time-blocks";
 
-import { timeBlockService } from '@/services/time-block.service'
+import { timeBlockService } from "@/services/time-block.service";
 
 export function useTimeBlockDnd(
-	items: ITimeBlockResponse[] | undefined,
-	setItems: Dispatch<SetStateAction<ITimeBlockResponse[] | undefined>>
+  items: ITimeBlockResponse[] | undefined,
+  setItems: Dispatch<SetStateAction<ITimeBlockResponse[] | undefined>>,
 ) {
-	const sensors = useSensors(
-		useSensor(PointerSensor),
-		useSensor(KeyboardSensor)
-	)
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor),
+  );
 
-	const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
-	const { mutate } = useMutation({
-		mutationKey: ['update order time block'],
-		mutationFn: (ids: string[]) => timeBlockService.updateOrderTimeBlock(ids),
-		onSuccess() {
-			queryClient.invalidateQueries({ queryKey: ['time-blocks'] })
-		}
-	})
+  const { mutate } = useMutation({
+    mutationKey: ["update order time block"],
+    mutationFn: (ids: string[]) => timeBlockService.updateOrderTimeBlock(ids),
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["time-blocks"] });
+    },
+  });
 
-	const handleDragEnd = (event: DragEndEvent) => {
-		const { active, over } = event
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
 
-		if (active.id !== over?.id && items) {
-			const oldIndex = items.findIndex(item => item.id === active.id)
-			const newIndex = items.findIndex(item => item.id === (over?.id || ''))
+    if (active.id !== over?.id && items) {
+      const oldIndex = items.findIndex((item) => item.id === active.id);
+      const newIndex = items.findIndex((item) => item.id === (over?.id || ""));
 
-			if (oldIndex !== -1 && newIndex !== -1) {
-				// Создаем новый отсортированный массив
-				const newItems = arrayMove(items, oldIndex, newIndex)
-				// Обновляем состояние
-				setItems(newItems)
-				// Обновляем порядок на сервере
-				mutate(newItems.map(item => item.id))
-			}
-		}
-	}
+      if (oldIndex !== -1 && newIndex !== -1) {
+        // Создаем новый отсортированный массив
+        const newItems = arrayMove(items, oldIndex, newIndex);
+        // Обновляем состояние
+        setItems(newItems);
+        // Обновляем порядок на сервере
+        mutate(newItems.map((item) => item.id));
+      }
+    }
+  };
 
-	return { handleDragEnd, sensors }
+  return { handleDragEnd, sensors };
 }
